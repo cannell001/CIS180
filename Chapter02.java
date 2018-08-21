e32300ac6cf33ab198d593687824fc96cc8d6abb
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:45:14 -0400

    Payroll.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,14 +1,38 @@
-// This example is much more readable than Compact.java.
+import java.util.Scanner;  // Needed for the Scanner class
 
-public class Readable
+/**
+ *   This program demonstrates the Scanner class.
+ */
+
+public class Payroll
 {
    public static void main(String[] args)
    {
-      int shares = 220;
-      double averagePrice = 14.67;
+      String name;         // To hold a name
+      int hours;           // Hours worked
+      double payRate,      // Hourly pay rate
+             grossPay;     // Gross pay
 
-      System.out.println("There were " + shares +
-                         " shares sold at $" +
-                         averagePrice + " per share.");
+      // Create a Scanner object to read input.
+      Scanner keyboard = new Scanner(System.in);
+
+      // Get the user's name.
+      System.out.print("What is your name? ");
+      name = keyboard.nextLine();
+
+      // Get the number of hours worked this week.
+      System.out.print("How many hours did you work this week? ");
+      hours = keyboard.nextInt();
+
+      // Get the user's hourly pay rate.
+      System.out.print("What is your hourly pay rate? ");
+      payRate = keyboard.nextDouble();
+
+      // Calculate the gross pay.
+      grossPay = hours * payRate;
+
+      // Display the resulting information.
+      System.out.println("Hello " + name);
+      System.out.println("Your gross pay is $" + grossPay);
    }
 }
