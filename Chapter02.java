906cd7224910f4a9cb5a47bc2852ac8cb491ef77
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 15:03:23 -0400

    CurrencyFormat2.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,23 +1,18 @@
-/*
-* This program displays a variety of
-* numbers left-justified in columns
-*/
+import javax.swing.JOptionPane;
 
-public class LeftJustified {
+/**
+ *  This program demonstrates how to use the String.format
+ *  method to format a number as currency.
+ */
 
-    public static void main(String args[]) {
-        // declare a variety of double variables
-        int num1 = 123;
-        int num2 = 12;
-        int num3 = 45678;
-        int num4 = 456;
-        int num5 = 1234567;
-        int num6 = 1234;
+public class CurrencyFormat2
+{
+   public static void main(String[] args)
+   {
+      double monthlyPay = 5000.0;
+      double annualPay = monthlyPay * 12;
+      String output = String.format("Your annual pay is $%,.2f", annualPay);
 
-        //Display each variable left-justified
-        //in a field of 8 spaces
-        System.out.printf("%-8d%-8d\n", num1, num2);
-        System.out.printf("%-8d%-8d\n", num3, num4);
-        System.out.printf("%-8d%-8d\n", num5, num6);
-    }
+      JOptionPane.showMessageDialog(null, output);
+   }
 }
