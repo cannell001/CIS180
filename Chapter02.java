977601909ba0afd5f02d015f9141ebc886220c10
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:03:40 -0400

    Variables.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,11 +1,15 @@
 // Another well adjusted printing program
 
-public class Tabs
+// This program has a variable.
+
+public class Variables
 {
    public static void main(String[] args)
    {
-      System.out.print("These are our top sellers:\n");
-      System.out.print("\tComputer games\n\tCoffee\n");
-      System.out.println("\tAspirin");
+      int value;
+
+      value = 5;
+      System.out.print("The value is ");
+      System.out.println(value);
    }
 }
