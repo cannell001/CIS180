d894c37248bb91dc2b0e31faccd4d7ea5792f62a
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:52:23 -0400

    NamesDialog.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,38 +1,27 @@
-import java.util.Scanner;  // Needed for the Scanner class
+import javax.swing.JOptionPane;
 
 /**
- *   This program correctly read numeric and string input.
+ *  This program demonstrates using dialogs
+ *  with JOptionPane.
  */
 
-public class CorrectedInputProblem
+public class NamesDialog
 {
    public static void main(String[] args)
    {
-      String name;   // To hold the user's name
-      int age;       // To hold the user's age
-      double income; // To hold the user's income
+      String firstName, lastName;
 
-      // Create a Scanner object to read input.
-      Scanner keyboard = new Scanner(System.in);
+      // Get the user's first name.
+      firstName =
+         JOptionPane.showInputDialog("What is your first name?");
 
-      // Get the user's age.
-      System.out.print("What is your age? ");
-      age = keyboard.nextInt();
+      // Get the user's last name.
+      lastName =
+         JOptionPane.showInputDialog("What is your last name?");
 
-      // Get the user's income
-      System.out.print("What is your annual income? ");
-      income = keyboard.nextDouble();
-
-      // Consume the remaining newline.
-      keyboard.nextLine();
-
-      // Get the user's name.
-      System.out.print("What is your name? ");
-      name = keyboard.nextLine();
-
-      // Display the information back to the user.
-      System.out.println("Hello " + name + ". Your age is "
-                         + age + " and your income is $"
-                         + income);
+      // Display a greeting
+      JOptionPane.showMessageDialog(null, "Hello " + firstName + " " +
+                                    lastName);
+      System.exit(0);
    }
 }
