b101d3322bdaba3c9c08894fc99057bcb70c23e7
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 12:56:40 -0400

    Tabs.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,11 +1,11 @@
-// An unruly printing program
+// Another well adjusted printing program
 
-public class Adjusted
+public class Tabs
 {
    public static void main(String[] args)
    {
       System.out.print("These are our top sellers:\n");
-      System.out.print("Computer games\nCoffee\n");
-      System.out.println("Aspirin");
+      System.out.print("\tComputer games\n\tCoffee\n");
+      System.out.println("\tAspirin");
    }
 }
