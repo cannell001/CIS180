84ca1f2f36d1b09e3402d8393ff38d9a377631df
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:37:17 -0400

    Comment1.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,10 +1,16 @@
-// This program can't find its variable.
+// PROGRAM: Comment1.java
+// Written by Herbert Dorfmann
+// This program calculates company payroll
+// Last modification: 8/30/2005
 
-public class Scope
+public class Comment1
 {
    public static void main(String[] args)
    {
-      System.out.println(value); // ERROR!
-      int value = 100;
+      double payRate;         // Holds the hourly pay rate
+      double hours;           // Holds the hours worked
+      int employeeNumber;     // Holds the employee number
+
+      // The Remainder of This Program is Omitted.
    }
 }
