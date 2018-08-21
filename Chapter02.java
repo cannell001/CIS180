850bf447b806e97c46e5b9131acaef277ef7f3a4
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:49:53 -0400

    InputProblem.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;  // Needed for the Scanner class
 
 /**
- *   This program demonstrates the Scanner class.
+ *   This program has a problem reading input.
  */
 
-public class Payroll
+public class InputProblem
 {
    public static void main(String[] args)
    {
-      String name;         // To hold a name
-      int hours;           // Hours worked
-      double payRate,      // Hourly pay rate
-             grossPay;     // Gross pay
+      String name;   // To hold the user's name
+      int age;       // To hold the user's age
+      double income; // To hold the user's income
 
       // Create a Scanner object to read input.
       Scanner keyboard = new Scanner(System.in);
+
+      // Get the user's age.
+      System.out.print("What is your age? ");
+      age = keyboard.nextInt();
+
+      // Get the user's income
+      System.out.print("What is your annual income? ");
+      income = keyboard.nextDouble();
 
       // Get the user's name.
       System.out.print("What is your name? ");
       name = keyboard.nextLine();
 
-      // Get the number of hours worked this week.
-      System.out.print("How many hours did you work this week? ");
-      hours = keyboard.nextInt();
-
-      // Get the user's hourly pay rate.
-      System.out.print("What is your hourly pay rate? ");
-      payRate = keyboard.nextDouble();
-
-      // Calculate the gross pay.
-      grossPay = hours * payRate;
-
-      // Display the resulting information.
-      System.out.println("Hello " + name);
-      System.out.println("Your gross pay is $" + grossPay);
+      // Display the information back to the user.
+      System.out.println("Hello " + name + ". Your age is "
+                         + age + " and your income is $"
+                         + income);
    }
 }
