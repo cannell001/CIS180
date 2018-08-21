121121f6bdf49c39d79ea46c51d0d3bbf6789d73
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 13:36:04 -0400

    Scope.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,19 +1,10 @@
-// This program demonstrates a few of the String methods.
+// This program can't find its variable.
 
-public class StringMethods
+public class Scope
 {
    public static void main(String[] args)
    {
-      String message = "Java is Great Fun!";
-      String upper = message.toUpperCase();
-      String lower = message.toLowerCase();
-      char letter = message.charAt(2);
-      int stringSize = message.length();
-
-      System.out.println(message);
-      System.out.println(upper);
-      System.out.println(lower);
-      System.out.println(letter);
-      System.out.println(stringSize);
+      System.out.println(value); // ERROR!
+      int value = 100;
    }
 }
