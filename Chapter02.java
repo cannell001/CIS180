72271e686400e8e8610d5399fe4708c498f50574
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-21 12:47:51 -0400

    GreatFun.java

--- Chapter02.java
+++ Chapter02.java
@@ -1,10 +1,10 @@
 // This is another simple Java program.
 
-public class TwoLines
+public class GreatFun
 {
    public static void main(String[] args)
    {
-      System.out.println("Programming is great fun!");
-      System.out.println("I can't get enough of it!");
+      System.out.print("Programming is ");
+      System.out.println("great fun!");
    }
 }
